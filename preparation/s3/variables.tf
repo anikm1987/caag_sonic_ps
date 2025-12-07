30e7ea68db4d0d6405a32548067cee748b17886b
Aniket Mukherjee <edde@mukherjee.com>
2025-12-07 18:00:29 +0000

    updated architecture and instructions

---
+++ preparation/s3/variables.tf
@@ -0,0 +1,14 @@
+variable "aws_region" {
+  description = "The AWS region to deploy to."
+  default     = "us-east-1"
+}
+
+variable "suffix" {
+  description = "The project name suffix."
+  default     = "dev"
+}
+
+variable "project_name" {
+  description = "The project name ."
+  default     = "caag-ps-nova"
+}
